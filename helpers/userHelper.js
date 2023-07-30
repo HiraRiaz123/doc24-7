036c6dd1bf9aa903345b0c004a2af3e86453bf87
hira <hirariaz905@gmail.com>
2023-07-31 01:52:50 +0500

    Initial commit

--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -6,7 +6,6 @@ const readUserData = (callback) => {
     if (err && err.code !== "ENOENT") {
       return callback(err);
     }
-
     let users = [];
     if (data) {
       try {
@@ -15,7 +14,6 @@ const readUserData = (callback) => {
         return callback(error);
       }
     }
-
     callback(null, users);
   });
 };
@@ -29,63 +27,28 @@ const writeUserData = (users, callback) => {
       if (err) {
         return callback(err);
       }
-
       callback(null);
     }
   );
 };
+
 const isNotNumeric = (value) => {
   return isNaN(value);
 };
+
 const validateUser = [
   check("id").notEmpty().withMessage("ID is required."),
-  check("email")
-    .isEmail()
-    .withMessage("Invalid email format.")
-    .bail()
-    .isLength({ max: 255 })
-    .withMessage("Email must be less than 256 characters.")
-    .custom(async (email) => {
-      return true;
-    }),
-  check("password")
-    .notEmpty()
-    .withMessage("Password is required.")
-    .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters long."),
-  check("name")
-    .notEmpty()
-    .withMessage("Name is required.")
-    .custom(isNotNumeric)
-    .withMessage("Name must not be numeric.")
-    .isLength({ min: 3 })
-    .withMessage("Name must be at least 3 characters long."),
-  check("age")
-    .notEmpty()
-    .withMessage("Age is required.")
-    .isInt()
-    .withMessage("Age must be an integer."),
-  check("weight")
-    .notEmpty()
-    .withMessage("Weight is required.")
-    .isInt()
-    .withMessage("Weight must be an integer."),
-  check("height")
-    .notEmpty()
-    .withMessage("Height is required.")
-    .isFloat()
-    .withMessage("Height must be a floating-point number."),
-  check("gender")
-    .notEmpty()
-    .withMessage("Gender is required.")
-    .isIn(["male", "female"])
-    .withMessage('Gender must be either "male" or "female".'),
-  check("isMarried")
-    .notEmpty()
-    .withMessage("Status is required.")
-    .isBoolean()
-    .withMessage("Status must be a boolean (true/false)."),
+  check("email").isEmail().withMessage("Invalid email format.").bail().isLength({ max: 255 }).withMessage("Email must be less than 256 characters.")
+  .custom(async (email) => {  return true;}),
+  check("password").notEmpty().withMessage("Password is required.").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long."),
+  check("name").notEmpty().withMessage("Name is required.").custom(isNotNumeric).withMessage("Name must not be numeric.").isLength({ min: 3 }).withMessage("Name must be at least 3 characters long."),
+  check("age").notEmpty().withMessage("Age is required.").isInt().withMessage("Age must be an integer."),
+  check("weight").notEmpty().withMessage("Weight is required.").isInt().withMessage("Weight must be an integer."),
+  check("height").notEmpty().withMessage("Height is required.").isFloat().withMessage("Height must be a floating-point number."),
+  check("gender").notEmpty().withMessage("Gender is required.").isIn(["male", "female"]).withMessage('Gender must be either "male" or "female".'),
+  check("isMarried").notEmpty().withMessage("Status is required.").isBoolean().withMessage("Status must be a boolean (true/false)."),
 ];
+
 const validationErrors = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -98,8 +61,5 @@ const validationErrors = (req, res, next) => {
 };
 
 module.exports = {
-  readUserData,
-  writeUserData,
-  validateUser,
-  validationErrors,
+  readUserData, writeUserData, validateUser, validationErrors,
 };
